8a939ba14d634ef53dde14ecc577a0934edda1f4
ClayGuess <guessclay@Yahoo.com>
2020-07-27 12:49:26 -0400

    created serializers.py file

---
+++ providence_app/serializers.py
@@ -0,0 +1,22 @@
+  from rest_framework import serializers
+from .models import Truck, Load, Dispatch
+
+class TruckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Truck
+        fields = ('id', 'name', 'trailertype', 'trailernum',
+         'hours', 'Phone', 'trucknum', 'drivernum', 'email')
+
+class LoadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Load
+        fields = ('id', 'loadnum', 'pickupnum', 'deliverynum', 
+        'pickuptime', 'deliverytime', 'rate', 'miles', 
+        'pickuploc', 'deliveryloc', 'droppick', 'dropdel')
+
+class DispatchSerializer(serializers.ModelSerializer):
+    loadinfo = LoadSerializer()
+    truckinfo = TruckSerializer()
+    class Meta:
+        model = Dispatch
+        fields = ('id', 'truckinfo', 'loadinfo')
