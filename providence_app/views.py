1aed8c790a6a119afa58fcc517f66d6995fc76c2
ClayGuess <guessclay@Yahoo.com>
2020-07-27 12:54:01 -0400

    updated views.py folder in providence_app directory

--- providence_app/views.py
+++ providence_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TruckSerializer, LoadSerializer, DispatchSerializer
+from .models import Truck, Load, Dispatch
 
-# Create your views here.
+
+class TruckView(viewsets.ModelViewSet):
+    queryset = Truck.objects.all()
+    serializer_class = TruckSerializer
+
+class LoadView(viewsets.ModelViewSet):
+    queryset = Load.objects.all()
+    serializer_class = LoadSerializer
+
+class DispatchView(viewsets.ModelViewSet):
+    queryset = Dispatch.objects.all()
+    serializer_class = DispatchSerializer
