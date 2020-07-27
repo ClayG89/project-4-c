5ff2fe5eb56f2c541fcdded072c7b182aba67130
ClayGuess <guessclay@Yahoo.com>
2020-07-27 17:11:24 -0400

    Imported components and added routes to app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from "./components/Home";
+import TruckList from "./components/TruckList";
+import Truck from "./components/Truck";
+import LoadList from "./components/LoadList";
+import Load from "./components/Load";
+import DispatchList from "./components/DispatchList";
+import Dispatch from "./components/Dispatch";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/truck" component={TruckList}/>
+            <Route path="/truck/:id" component={Truck}/>
+            <Route exact path="/load" component={LoadList}/>
+            <Route path="/load/:id" component={Load}/>
+            <Route exact path="/dispatch" component={DispatchList}/>
+            <Route path="/dispatch/:id" component={Dispatch}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
